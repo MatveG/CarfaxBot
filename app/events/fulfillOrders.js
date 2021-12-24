b17d31a2a00249e1cd007713680c14bc72538b9c
Serhii Kasianov <matvik.net@gmail.com>
2021-12-24 13:48:52 +0200

    Bad file issue fix

--- app/events/fulfillOrders.js
+++ app/events/fulfillOrders.js
@@ -2,8 +2,9 @@ import fs from 'fs';
 import config from '../loaders/config';
 import i18n from '../loaders/i18n';
 import {sendDocument, sendMessage} from '../utils/sendTelegram';
-import {select, remove} from '../utils/nedbOrm';
+import {select, remove, update} from '../utils/nedbOrm';
 import sendMail from '../utils/sendMail';
+import logger from '../loaders/logger';
 
 export default async () => {
   const orders = await select('orders', {status: {$gt: 0}});
@@ -12,18 +13,26 @@ export default async () => {
     const fileName = `${vin}.${translate ? 'rus.pdf' : 'pdf'}`;
     const filePath = `${config.archive}/${fileName}`;
 
-    if (status === 4 && fs.existsSync(filePath)) {
-      await sendDocument(chatId, `${vin}.pdf`, filePath);
-      await sendMessage(chatId, i18n.t(locale, 'order.finish'));
+    if (attempts > 40 && Date.now()-paid > 15*60*1000) {
+      await sendMessage(chatId, i18n.t(locale, 'order.error', {vin}));
       await remove('orders', _id);
+      return;
+    }
+
+    if (status === 4 && fs.existsSync(filePath)) {
+      try {
+        await sendDocument(chatId, `${vin}.pdf`, filePath);
+        await sendMessage(chatId, i18n.t(locale, 'order.finish'));
+        await remove('orders', _id);
+      } catch (e) {
+        await update('orders', {_id}, {attempts: attempts + 1});
+        logger.error(e);
+      }
 
       if (Object.keys(contacts).length) {
         const {phone, method} = contacts;
         await notifyAboutCallback(phone, method, vin, fileName, filePath);
       }
-    } else if (attempts > 40 && Date.now()-paid > 10*60*1000) {
-      await sendMessage(chatId, i18n.t(locale, 'order.error', {vin}));
-      await remove('orders', _id);
     }
   }
 };
